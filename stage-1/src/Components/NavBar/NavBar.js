9189c4df8d9bf2087cac6d90ca2884f1de72e13f
unknown <cambuf@gmail.com>
2020-02-06 12:28:44 -0700

    final

--- stage-1/src/Components/NavBar/NavBar.js
+++ stage-1/src/Components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './NavBar.css';
-import Logo from './../dev-mountain-logo.png'
+
+import Logo from './dev-mountain-logo.png'
 
 class NavBar extends Component {
   render() {
